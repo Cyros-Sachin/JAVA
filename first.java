d8d0b6a3585502cace2c87f7c697fdabc9c9244f
Kumar Sachin <sachinkc4456@gmail.com>
2025-04-09 02:10:03 +0530

    yup

--- first.java
+++ first.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.*;
 
 class first{
     public static void main(String args[]){
-        System.out.println("Enter the radius of circle:");
-        @SuppressWarnings("resource")
-        Scanner sc = new Scanner(System.in);
-        int radius = sc.nextInt();
-        double area = 3.14 * radius * radius;
-        System.out.println("Area of circle is:"+area);
+        int n = 64;
+        n=n>>2;
+        
+        System.out.println(n);
     }
 }
